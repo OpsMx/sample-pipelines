f46613d903056d5e60993826d0712d8e3a8f952f
yeshaswini_opsmx <68692913+yeshaswiniOpsmx@users.noreply.github.com>
2021-02-02 19:09:15 +0530

    Update create-app.sh

--- create-app.sh
+++ create-app.sh
@@ -1,6 +1,6 @@
-spin application save -f /home/yeshaswini/LCM/sample-app/sample-application.json
-spin pi save -f /home/yeshaswini/LCM/sample-app/opa-pipeline.json
-spin pi save -f /home/yeshaswini/LCM/sample-app/git-trigger-deploy.json
-spin pi save -f /home/yeshaswini/LCM/sample-app/K8s-deploy-manifest.json
-spin pi save -f /home/yeshaswini/LCM/sample-app/docker-trigger.json
-spin pi save -f /home/yeshaswini/LCM/sample-app/k8s-deploy-git-manifest.json
+spin application save -f /filepath/sample-application.json
+spin pi save -f /filepath/opa-pipeline.json
+spin pi save -f /filepath/git-trigger-deploy.json
+spin pi save -f /filepath/K8s-deploy-manifest.json
+spin pi save -f /filepath/docker-trigger.json
+spin pi save -f /filepath/k8s-deploy-git-manifest.json
