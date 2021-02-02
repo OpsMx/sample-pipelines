26e51e92452a127d5350de8a3bec11fe12e8b22a
Yeshaswini.M <yeshaswini.m@opsmx.io>
2021-02-02 18:01:24 +0530

    Adding pipeline Json files

---
+++ create-app.sh
@@ -0,0 +1,6 @@
+spin application save -f /home/yeshaswini/LCM/sample-app/sample-application.json
+spin pi save -f /home/yeshaswini/LCM/sample-app/opa-pipeline.json
+spin pi save -f /home/yeshaswini/LCM/sample-app/git-trigger-deploy.json
+spin pi save -f /home/yeshaswini/LCM/sample-app/K8s-deploy-manifest.json
+spin pi save -f /home/yeshaswini/LCM/sample-app/docker-trigger.json
+spin pi save -f /home/yeshaswini/LCM/sample-app/k8s-deploy-git-manifest.json
