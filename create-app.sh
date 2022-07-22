0b98d409d994e09f77f3951ed3c4c69d5a79cbf8
ksrinimba <36603781+ksrinimba@users.noreply.github.com>
2022-07-22 12:26:21 +0530

    Update create-app.sh

--- create-app.sh
+++ create-app.sh
@@ -62,5 +62,5 @@ spin pi save -f pipeline-jsonfile/Restore-minio.json
 spin pi save -f pipeline-jsonfile/byos-helm-reinstall.json
 spin pi save -f pipeline-jsonfile/byos-helm-setup.json
 spin pi save -f pipeline-jsonfile/byos-helm-upgrade-preview.json
-spin pi save -f pipeline-jsonfile/DB-migrate
+spin pi save -f pipeline-jsonfile/DB-migrate.json
 spin pi save -f pipeline-jsonfile/DB-migration-39to310.json
